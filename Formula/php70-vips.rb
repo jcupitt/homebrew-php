18f02ea541ee9001d72e696a976d41e6763f6b75
John Cupitt <jcupitt@gmail.com>
2016-11-10 12:11:53 +0000

    rename to php70-vips, fix reference to vips
    * rename to php70-vips to that the library is named correctly
    * add the tap prefix to the vips dependency

--- Formula/php70-vips.rb
+++ Formula/php70-vips.rb
@@ -1,16 +1,16 @@
 require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 
-class Php70VipsExt < AbstractPhp70Extension
+class Php70Vips < AbstractPhp70Extension
   init
   desc "Native part of php-vips"
   homepage "https://github.com/jcupitt/php-vips-ext"
-  url "https://github.com/jcupitt/php-vips-ext/archive/v0.1.2.tar.gz"
-  sha256 "72d7e821d2899729de0f9ff616350721d12371e08016f069f5b5836837f5e859"
+  url "https://github.com/jcupitt/php-vips-ext/archive/v0.1.3.tar.gz"
+  sha256 "dfd11b502e00ce7e406cf8f82fe48fe9612df1a818d3dc6cb4c8561092699ae8"
   head "https://github.com/jcupitt/php-vips-ext.git"
 
   depends_on "pkg-config" => :build
 
-  depends_on "vips"
+  depends_on "homebrew/science/vips"
 
   def install
     ENV.universal_binary if build.universal?
