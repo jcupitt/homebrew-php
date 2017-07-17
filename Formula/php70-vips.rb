4c12e61de08e941d36359211e4f6a11cab320720
John Cupitt <jcupitt@gmail.com>
2017-07-17 13:59:27 +0100

    update formula to latest php-vips-ext
    changes:
    
    - update to latest version of php-vips-ext
    - download from pecl, not github
    - changes to please brew audit
    - depend on vips in homebrew-core

--- Formula/php70-vips.rb
+++ Formula/php70-vips.rb
@@ -3,26 +3,21 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php70Vips < AbstractPhp70Extension
   init
   desc "Native part of php-vips"
-  homepage "https://github.com/jcupitt/php-vips-ext"
-  url "https://github.com/jcupitt/php-vips-ext/archive/v1.0.2.tar.gz"
-  sha256 "52d005ecc18930c97e1927c6a8b5d9390614946520b895fea42a5b790e34c4df"
+  homepage "https://pecl.php.net/package/vips"
+  url "https://pecl.php.net/get/vips-1.0.7.tgz"
+  sha256 "ddc3f396237af05ee4ef5f207b004c7b387b28736d9c1878dd24475a807c5337"
   head "https://github.com/jcupitt/php-vips-ext.git"
 
   depends_on "pkg-config" => :build
-
-  depends_on "homebrew/science/vips"
+  depends_on "vips"
 
   def install
-    ENV.universal_binary if build.universal?
+    Dir.chdir "vips-#{version}" unless build.head?
 
     args = []
     args << "--with-vips"
-
     safe_phpize
-
-    system "./configure", "--prefix=#{prefix}",
-                          phpconfig,
-                          *args
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
     system "make"
     prefix.install "modules/vips.so"
     write_config_file if build.with? "config-file"
